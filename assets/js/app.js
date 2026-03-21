640cf2ef3a71c3ac27787fd2b023323a4d3be05e
BossCyr <ahomadegbeprince19@gmail.com>
2026-03-21 20:22:15 +0100

    Refonte de sjmv

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,5 @@ const navLinks = document.querySelector(".nav-links");
 
 toggle.addEventListener("click", () =>  {
     navLinks.classList.toggle("active");
+    toggle.classList.toggle("is-active");
 });
